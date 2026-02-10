12b97d3804133af88db763f33415e4026360da5d
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-10 21:07:53 +0800

    refactor: change the birth year form for existing authors usign dropdown

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,11 +1,12 @@
 import { useQuery, useMutation } from '@apollo/client/react';
 import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries';
 import { useState } from 'react';
+import Select from 'react-select';
 
 const Authors = (props) => {
-	const [author, setAuthor] = useState('');
 	const [bornTo, setBornTo] = useState('');
 	const [errorMessage, setErrorMessage] = useState(null);
+	const [selectedOption, setSelectedOption] = useState(null);
 	const result = useQuery(ALL_AUTHORS);
 	const [editAuthor] = useMutation(EDIT_AUTHOR, {
 		refetchQueries: [{ query: ALL_AUTHORS }],
@@ -18,14 +19,15 @@ const Authors = (props) => {
 		setErrorMessage(null);
 
 		const born = Number(bornTo);
-		if (!author.trim() || isNaN(born)) {
+		if (!selectedOption || isNaN(born)) {
 			setErrorMessage('Author name and birth year are required');
 			return;
 		}
 
 		try {
-			await editAuthor({ variables: { name: author, setBornTo: born } });
-			setAuthor('');
+			await editAuthor({
+				variables: { name: selectedOption.value, setBornTo: born },
+			});
 			setBornTo('');
 		} catch (error) {
 			setErrorMessage(error.message);
@@ -38,6 +40,10 @@ const Authors = (props) => {
 	if (result.loading) return <div>loading...</div>;
 	if (result.error) return <div>error: {result.error.message}</div>;
 	const authors = result.data.allAuthors;
+	const options = authors.map((author) => ({
+		value: author.name,
+		label: author.name,
+	}));
 
 	return (
 		<div>
@@ -64,9 +70,13 @@ const Authors = (props) => {
 				<form onSubmit={submit}>
 					<div>
 						name
-						<input
-							value={author}
-							onChange={({ target }) => setAuthor(target.value)}
+						<Select
+							defaultValue={selectedOption}
+							onChange={setSelectedOption}
+							options={options}
+							styles={{
+								container: (base) => ({ ...base, width: 200, height: 50 }),
+							}}
 						/>
 					</div>
 					<div>
