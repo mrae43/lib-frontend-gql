54bc304510f90b1d9d991f60d65bda68ced3166c
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-10 11:01:59 +0800

    chore: add initial setup for the Apollo Client in top level

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+import { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+	link: new HttpLink({
+		uri: 'http://localhost:4000',
+	}),
+	cache: new InMemoryCache(),
+});
+
+const ALL_AUTHORS = gql`
+	query {
+		allAuthors {
+			name
+			born
+			bookCount
+		}
+	}
+`;
+
+const ALL_BOOKS = gql`
+	query {
+		allBooks {
+			title
+			published
+			author {
+				name
+			}
+		}
+	}
+`;
+
+client.query({ ALL_AUTHORS, ALL_BOOKS }).then((response) => {
+	console.log(response.data);
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+	<StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</StrictMode>,
+);
