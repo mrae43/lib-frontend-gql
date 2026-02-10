6400a5ae24dfb44feee10e439a93724e2e02408c
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-10 16:15:50 +0800

    fix: remove unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-import { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -11,32 +11,6 @@ const client = new ApolloClient({
 		uri: 'http://localhost:4000',
 	}),
 	cache: new InMemoryCache(),
-});
-
-const ALL_AUTHORS = gql`
-	query {
-		allAuthors {
-			name
-			born
-			bookCount
-		}
-	}
-`;
-
-const ALL_BOOKS = gql`
-	query {
-		allBooks {
-			title
-			published
-			author {
-				name
-			}
-		}
-	}
-`;
-
-client.query({ query: ALL_AUTHORS }).then((response) => {
-	console.log(response.data);
 });
 
 createRoot(document.getElementById('root')).render(
