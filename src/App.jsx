604cdb3b0559adf6d1570c7215b49cf1ed9fc331
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-12 14:51:38 +0800

    fix: refactor the App component for the access route, feat: implement logout functionality, refactor: error message to the parent component'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+import { useApolloClient } from '@apollo/client/react';
 
 import Authors from './components/Authors';
 import Books from './components/Books';
@@ -8,29 +9,83 @@ import LoginForm from './components/LoginForm';
 
 const App = () => {
 	const [page, setPage] = useState('authors');
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [token, setToken] = useState(
+		localStorage.getItem('library-user-token'),
+	);
+
+	const client = useApolloClient();
+
+	const onLogout = () => {
+		setToken(null);
+		localStorage.clear();
+		client.resetStore();
+	};
+
+	const notify = (message) => {
+		setErrorMessage(message);
+		setTimeout(() => {
+			setErrorMessage(null);
+		}, 10000);
+	};
 
 	return (
 		<div>
-			<div>
-				<Link to={'/'}>
-					<button onClick={() => setPage('authors')}>authors</button>
-				</Link>
-				<Link to={'/books'}>
-					<button onClick={() => setPage('books')}>books</button>
-				</Link>
-				<Link to={'/add_book'}>
-					<button onClick={() => setPage('add')}>add book</button>
-				</Link>
-				<Link to={'/login'}>
-					<button onClick={() => setPage('login')}>login</button>
-				</Link>
-			</div>
+			{token && (
+				<div>
+					<Link to={'/'}>
+						<button onClick={() => setPage('authors')}>authors</button>
+					</Link>
+					<Link to={'/books'}>
+						<button onClick={() => setPage('books')}>books</button>
+					</Link>
+					<Link to={'/add_book'}>
+						<button onClick={() => setPage('add')}>add book</button>
+					</Link>
+					<button onClick={onLogout}>logout</button>
+				</div>
+			)}
+
 			<Routes>
-				<Route path='/' element={<Authors show={page === 'authors'} />} />
-				<Route path='/books' element={<Books show={page === 'books'} />} />
-				<Route path='/add_book' element={<NewBook show={page === 'add'} />} />
-				<Route path='/login' element={<LoginForm show={page === 'login'} />} />
+				<Route
+					path='/'
+					element={
+						token ? (
+							<Authors show={page === 'authors'} />
+						) : (
+							<Navigate to={'/login'} />
+						)
+					}
+				/>
+				{/* Protected Routes */}
+				<Route
+					path='/books'
+					element={
+						token ? (
+							<Books show={page === 'books'} />
+						) : (
+							<Navigate to={'/login'} />
+						)
+					}
+				/>
+				<Route
+					path='/add_book'
+					element={
+						token ? (
+							<NewBook show={page === 'add'} />
+						) : (
+							<Navigate to={'/login'} />
+						)
+					}
+				/>
+				<Route
+					path='/login'
+					element={<LoginForm setError={notify} setToken={setToken} />}
+				/>
 			</Routes>
+			{errorMessage && (
+				<div style={{ color: 'red', marginTop: '1rem' }}>{errorMessage}</div>
+			)}
 		</div>
 	);
 };
