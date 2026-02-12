8b31c90a35e1626ece28c64b84c4df554207fe16
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-12 13:56:22 +0800

    feat: initialize login core functionality

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client/react';
+import { LOGIN } from '../queries';
+
 const LoginForm = () => {
-	return <div>Login</div>;
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [errorMessage, setErrorMessage] = useState(null);
+
+  const [login] = useMutation(LOGIN, {
+    
+  });
+
+	const submit = async (event) => {
+		event.preventDefault();
+
+		try {
+			await login({ variables: { username, password } });
+			console.log(`name: ${username}, password: ${password}`);
+		} catch (error) {
+			setErrorMessage(error.message);
+		}
+	};
+
+	return (
+		<div>
+			{errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+			<form onSubmit={submit}>
+				<div>
+					username
+					<input
+						type='text'
+						value={username}
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+				</div>
+				<div>
+					password
+					<input
+						type='password'
+						value={password}
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</div>
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	);
 };
 
 export default LoginForm;
