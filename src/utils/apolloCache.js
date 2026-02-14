e6f7050d927cc11ad3aba082ee935d45a31f5e87
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-14 20:16:14 +0800

    fix: update the utils for apollo cache to accept genre as variables

--- src/utils/apolloCache.js
+++ src/utils/apolloCache.js
@@ -1,7 +1,9 @@
 import { ALL_BOOKS } from '../queries';
 
-export const addBookToCache = (cache, bookToAdd) => {
-	cache.updateQuery({ query: ALL_BOOKS }, ({ allBooks }) => {
+export const addBookToCache = (cache, bookToAdd, currentGenre) => {
+	const variables =
+		currentGenre === 'all' ? { genre: null } : { genre: currentGenre };
+	cache.updateQuery({ query: ALL_BOOKS, variables }, ({ allBooks }) => {
 		const bookExists = allBooks.some((book) => book.id === bookToAdd.id);
 
 		if (bookExists) {
